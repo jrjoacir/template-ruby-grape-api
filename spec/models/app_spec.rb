171e7787bf8a7cb8dc178e9aa5d23ab9439d4e3f
Joacir Junior <jr.joacir@gmail.com>
2019-01-01 12:12:50 -0200

    Created app model test

---
+++ spec/models/app_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe Models::App do
+  context '#initialize' do
+    let(:app) { Models::App.new(app_hash) }
+    let(:app_hash) { { name: 'App', description: 'Description' } }
+
+    it 'app returns name' do
+      expect(app.name).to eq app_hash[:name]
+    end
+
+    it 'app returns description' do
+      expect(app.description).to eq app_hash[:description]
+    end
+  end
+end
