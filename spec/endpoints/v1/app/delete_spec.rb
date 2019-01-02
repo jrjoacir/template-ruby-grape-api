ff7b8e24d65232a9393d8fe6429315e7352b59b1
Joacir Junior <jr.joacir@gmail.com>
2019-01-02 01:52:25 -0200

    Implemented test for endpoint app delete

---
+++ spec/endpoints/v1/app/delete_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Endpoints::V1::App::Delete do
+  before { delete("v1/app/#{id}") }
+
+  context 'when exist an app' do
+    let(:id) { FactoryBot.create(:app).id }
+
+    it 'return http status 204 - NoContent' do
+      expect(last_response.status).to eq 204
+    end
+
+    it 'return app response body' do
+      expect(last_response.body).to be_empty
+    end
+  end
+
+  context 'when no exist app' do
+    let(:id) { 1 }
+    let(:response_body) { { message: 'App does not exist' }.to_json }
+
+    it 'return http status 404 - Notfound' do
+      expect(last_response.status).to eq 404
+    end
+
+    it 'return error response body' do
+      expect(last_response.body).to eq response_body
+    end
+  end
+end
--- spec/endpoints/v1/app/get_spec.rb
+++ spec/endpoints/v1/app/get_spec.rb
@@ -1,32 +1,30 @@
 RSpec.describe Endpoints::V1::App::Get do
-  context '#get' do
-    subject { get('v1/app') }
+  subject { get('v1/app') }
 
-    context 'when exist an app' do
-      before { application }
+  context 'when exist an app' do
+    before { application }
 
-      let(:application) { FactoryBot.create(:app) }
-      let(:response_body) { application.to_hash.to_json }
+    let(:application) { FactoryBot.create(:app) }
+    let(:response_body) { application.to_hash.to_json }
 
-      it 'return http status 200 - OK' do
-        expect(subject.status).to eq 200
-      end
-
-      it 'return app response body' do
-        expect(subject.body).to eq response_body
-      end
+    it 'return http status 200 - OK' do
+      expect(subject.status).to eq 200
     end
 
-    context 'when no exist app' do
-      let(:response_body) { { message: 'App does not exist' }.to_json }
+    it 'return app response body' do
+      expect(subject.body).to eq response_body
+    end
+  end
 
-      it 'return http status 404 - Notfound' do
-        expect(subject.status).to eq 404
-      end
+  context 'when no exist app' do
+    let(:response_body) { { message: 'App does not exist' }.to_json }
 
-      it 'return error response body' do
-        expect(subject.body).to eq response_body
-      end
+    it 'return http status 404 - Notfound' do
+      expect(subject.status).to eq 404
+    end
+
+    it 'return error response body' do
+      expect(subject.body).to eq response_body
     end
   end
 end
