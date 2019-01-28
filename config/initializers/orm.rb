ecd2b51d172fc65dd4c509d5fa5330a4100bf0e8
Joacir Junior <jr.joacir@gmail.com>
2019-01-27 23:00:15 -0200

    Created a timeout (60 seconds) for connect database

--- config/initializers/orm.rb
+++ config/initializers/orm.rb
@@ -2,6 +2,7 @@ module ORM
   class Database
     ENVIRONMENT = ENV['RACK_ENV']
     DB_LOG_FILE = ENV['DB_LOG_FILE']
+    TIMEOUT_SECONDS = 60
 
     class << self
       def db
@@ -13,6 +14,16 @@ module ORM
       def migrate(version = nil)
         Sequel.extension :migration
         Sequel::Migrator.run(db, dir_migrations, target: version)
+      end
+
+      def connect_database(time = Time.now, attempt = 1)
+        ORM::Database.db
+      rescue StandardError => error
+        error_message = "Database is not available after #{attempt} attempts"
+        error_message += ", during #{TIMEOUT_SECONDS} seconds: (#{error})"
+        raise error, error_message if Time.now - time > TIMEOUT_SECONDS
+
+        connect_database(time, attempt + 1)
       end
 
       private
@@ -46,4 +57,4 @@ module ORM
   end
 end
 
-ORM::Database.db
+ORM::Database.connect_database
