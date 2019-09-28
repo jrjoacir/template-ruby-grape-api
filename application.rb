1bcb3f8275ec3f627d4b4a893945e36aeb101734
Joacir Oliveira Santos Júnior <joacir.junior@vagas.com.br>
2019-09-28 18:11:43 -0300

    Updated gem versions

--- application.rb
+++ application.rb
@@ -2,6 +2,6 @@
 
 require 'require_smasher'
 require_gems 'grape', 'grape-entity', 'grape_logging', 'pg', 'sequel', 'erb'
-require_gems 'yaml', 'dotenv', 'grape-swagger', 'grape-swagger-entity'
+require_gems 'yaml', 'grape-swagger', 'grape-swagger-entity'
 require_dirs 'config/initializers'
 require_file "config/environments/#{ENV['RACK_ENV']}"
